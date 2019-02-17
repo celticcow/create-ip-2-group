52a8a5a5a1d34a0e5350a1eddd4ff48b326462cd
Greg_Dunlap <47676148+celticcow@users.noreply.github.com>
2019-02-17 15:33:08 -0600

    version 1.1
    added domain login to create

--- create-host-add-2-group.sh
+++ create-host-add-2-group.sh
@@ -19,31 +19,31 @@ read GROUP
 
 printf "%s\n\n" "START"
 
-mgmt_cli -d $DOMAIN -r true login > id.txt
+mgmt_cli -d $DOMAIN -u $USER -p $PASS login > id.txt
 
 file="/home/admin/api/iplist-1.txt"
 
-mgmt_cli -d $DOMAIN -r true add group name $GROUP -s id.txt
+mgmt_cli -d $DOMAIN add group name $GROUP -s id.txt
 
 
 while IFS= read -r var
 do
-	ehost=$(mgmt_cli -d $DOMAIN -r true show objects type host filter $var ip-only true --format json | jq '.objects [] | .name')
+	ehost=$(mgmt_cli -d $DOMAIN -s id.txt show objects type host filter $var ip-only true --format json | jq '.objects [] | .name')
 
 	#printf "found $ehost\n"	
 	if [ -z $ehost ]; then
 		printf  "need to add $var\n"
-		mgmt_cli -d $DOMAIN -r true add host name c-$var ip-address $var groups.1 $GROUP -s id.txt
+		mgmt_cli -d $DOMAIN add host name c-$var ip-address $var groups.1 $GROUP -s id.txt
 	else
 		printf "found $ehost\n"
-		mgmt_cli -d $DOMAIN -r true set host name $ehost groups.1 $GROUP -s id.txt
+		mgmt_cli -d $DOMAIN set host name $ehost groups.1 $GROUP -s id.txt
 	fi
 
 done <"$file"
 
 
-mgmt_cli -d 146.18.96.25 -r true publish -s id.txt
-mgmt_cli -d 146.18.96.25 -r true logout -s id.txt
+mgmt_cli -d 146.18.96.25 publish -s id.txt
+mgmt_cli -d 146.18.96.25 logout -s id.txt
 
 
 printf "%s\n\n" "END"
